e50a8da6fb98ee4d98751981d8de5b62706b7705
Khamraj Rohit <Rohit.Khamraj@gmail.com>
2017-06-28 12:45:59 -0400

    Redesigned Audio Player

--- Aoba.Server/Bootstrap/RootProvider.cs
+++ Aoba.Server/Bootstrap/RootProvider.cs
@@ -12,7 +12,7 @@ namespace LuminousVector.Aoba.Server.Bootstrap
 	{
 		public string GetRootPath()
 		{
-			return @"C:\Users\Karuta\Documents\Visual Studio 2015\Projects\Aoba\Aoba.Server";
+			return @"C:\Users\Karuta\Documents\GitHub\Aoba\Aoba.Server";
 		}
 	}
 #endif
