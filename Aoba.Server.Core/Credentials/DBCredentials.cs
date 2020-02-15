4227b17f90cfcceca13102663e12bfb7d521df14
Khamraj Rohit <khamraj@kaisei.app>
2020-02-14 20:14:32 -0500

    mongodb url

--- Aoba.Server.Core/Credentials/DBCredentials.cs
+++ Aoba.Server.Core/Credentials/DBCredentials.cs
@@ -15,11 +15,8 @@ namespace LuminousVector.Aoba.Server.Credentials
 		public const string UserTable = "users";
 		public const string MediaTable = "images";
 		public const string RegTokenTable = "regtokens";
-#if DEBUG
 		public static string CONNECTION_STRING = $"mongodb://192.168.86.74:27017";
-#else
-		public static string CONNECTION_STRING = $"mongodb://localhost:27017";
-#endif
+		//public static string CONNECTION_STRING = $"mongodb://localhost:27017";
 		public static string PG_CONNECTION_STRING => $"Host={AobaCore.HOST};Username={DB_User};Password={DB_Pass};Database={DB_Name};Pooling=true";
 	}
 }
