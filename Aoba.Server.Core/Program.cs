7909d042f3bd0aa4f36ca870a0ef52cc534c5963
Khamraj Rohit <khamraj@kaisei.app>
2020-09-26 11:11:47 -0400

    loop

--- Aoba.Server.Core/Program.cs
+++ Aoba.Server.Core/Program.cs
@@ -1,20 +1,21 @@
+using Nancy.Hosting.Self;
+
 using System;
 using System.IO;
-using Nancy.Hosting.Self;
+using System.Threading;
 
 namespace LuminousVector.Aoba.Server
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
-			var host = new NancyHost(new HostConfiguration() { AllowChunkedEncoding = false, UrlReservations = new UrlReservations() { CreateAutomatically = true } }, new Uri("http://localhost:4321"));
+			using var host = new NancyHost(new HostConfiguration() { AllowChunkedEncoding = false, UrlReservations = new UrlReservations() { CreateAutomatically = true } }, new Uri("http://localhost:4321"));
 			host.Start();
 			Console.WriteLine(Directory.GetCurrentDirectory());
 			Console.WriteLine("Hosting on localhost:4321");
-			Console.ReadLine();
-			host.Dispose();
+			var ev = new ManualResetEvent(false);
+			ev.WaitOne();
 		}
 	}
-
 }
