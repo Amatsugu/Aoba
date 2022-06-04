3c6e5b1f7d0a387018ba8faaaf04180c5efbe4b2
Amatsugu <khamraj@kaisei.app>
2022-06-04 12:41:18 -0400

    add register

--- AobaServer/Controllers/AuthController.cs
+++ AobaServer/Controllers/AuthController.cs
@@ -22,8 +22,7 @@ namespace AobaServer.Controllers
 		[HttpGet("register/{token}")]
 		public IActionResult Register(string token)
 		{
-
-			return View();
+			return View(token);
 		}
 	}
 }
---
+++ AobaServer/wwwroot/js/Auth/register.js
@@ -0,0 +1,18 @@
+﻿$(() => {
+	var form = $("form");
+
+
+	var token = form.data("token");
+
+	form.on("submit", () => {
+
+		$.ajax({
+			url: `/api/auth/register/${token}`,
+			method: "POST",
+			data: form.serialize()
+		}).done(res => {
+			window.location.assign("/");
+		});
+
+	});
+});
